8200fd2d50fcb12e23b56b4f3b584e52902787df
Rekki <seraelreir@gmail.com>
2021-10-19 23:11:10 +0300

    2 new tests

---
+++ FSIS_autTest/ApproveHouseCrawlSpace.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FSIS_autTest
+{
+    public class ApproveHouseCrawlSpace
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://staging.floodsimple.com/quote");
+                //driver.Manage().Window.FullScreen();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div/input")).SendKeys("100 E CENTRAL BLVD, ORLANDO, FL 32801");
+                //Input Address
+                //You should change the address after running this test to avoid duplicate quotes
+                
+                DemoHelper.Pause();
+              
+
+                IWebElement continueButton = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button"));
+
+                continueButton.Click();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div[1]/div[2]/div[2]/input")).SendKeys("175000");
+                //Input Coverage
+                
+                DemoHelper.Pause();
+
+                IWebElement continueButton1 = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button"));
+
+                continueButton1.Click();
+
+                DemoHelper.Pause();
+
+                IWebElement propertyType = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/ul/li[1]/button"));
+
+                propertyType.Click();
+                //Select "House"
+                DemoHelper.Pause();
+
+                IWebElement TypeDropdown = driver.FindElement(By.ClassName("mat-select-arrow-wrapper"));
+
+                TypeDropdown.Click();
+                //Open dropdown list
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/mat-option[2]")).Click();
+                //Select "Residental - Renter Occupied"
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/ul/li[5]/button")).Click();
+                //Select "Home with Crawl Space"
+                DemoHelper.Pause();
+
+                driver.FindElement(By.ClassName("mat-select-arrow")).Click();
+                //Open dropdown list
+                DemoHelper.Pause();
+
+                driver.FindElement(By.Id("mat-option-7")).Click();
+                //Select "Crawlspace - Above Ground"
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/input")).SendKeys("1741");
+                //Input Square Footage
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/mat-radio-group/mat-radio-button[2]/label/div[1]")).Click();
+                //Flood Loss - NO
+                DemoHelper.Pause();
+
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button")).Click();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[1]/input")).SendKeys("HouseCrawlSpaceAboveG");
+                //Input First Name
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[2]/input")).SendKeys("HouseCrawlSpaceAboveG");
+                //Input Last Name
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[3]/input")).SendKeys("test@huisdigital.com");
+                //Input Email
+                DemoHelper.Pause();
+                //driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/button")).Click();
+                //Click "Email Quote"
+                DemoHelper.Pause(7000);
+            }
+        }
+    }
+}
---
+++ FSIS_autTest/ApproveTownHouseFullBasement.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FSIS_autTest
+{
+    public class ApproveTownHouseFullBasement
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadQuotePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://staging.floodsimple.com/quote");
+                //driver.Manage().Window.FullScreen();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div/input")).SendKeys("52960 JAYHAWK DR # 9, MCCONNELL AFB, KS 67221");
+                //Input Address
+                //You should change the address after running this test to avoid duplicate quotes
+                
+                DemoHelper.Pause();
+              
+
+                IWebElement continueButton = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button"));
+
+                continueButton.Click();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div[1]/div[2]/div[2]/input")).SendKeys("75000");
+                //Input Coverage
+                
+                DemoHelper.Pause();
+
+                IWebElement continueButton1 = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button"));
+
+                continueButton1.Click();
+
+                DemoHelper.Pause();
+
+                IWebElement propertyType = driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/ul/li[2]/button"));
+
+                propertyType.Click();
+                //Select "Condo/Townhouse"
+                DemoHelper.Pause();
+
+                IWebElement TypeDropdown = driver.FindElement(By.ClassName("mat-select-arrow"));
+
+                TypeDropdown.Click();
+                //Open dropdown list
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/mat-option[3]")).Click();
+                //Select "Townhouse"
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/ul/li[2]/button")).Click();
+                //Select "Home with Basement"
+                DemoHelper.Pause();
+
+                driver.FindElement(By.ClassName("mat-select-arrow-wrapper")).Click();
+                //Open dropdown list
+                DemoHelper.Pause();
+
+                driver.FindElement(By.ClassName("field-select2-option")).Click();
+                DemoHelper.Pause();
+                //Select "Full Basement"
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/input")).SendKeys("1500");
+               //Input Square Footage
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/mat-radio-group/mat-radio-button[2]/label/div[1]")).Click();
+                //Flood Loss - NO
+                DemoHelper.Pause();
+
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/button")).Click();
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[1]/input")).SendKeys("ApprTownhouseFBase");
+                //Input First Name
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[2]/input")).SendKeys("ApprTownhouseFBase");
+                //Input Last Name
+                driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/div[1]/div[3]/input")).SendKeys("test@huisdigital.com");
+                //Input Email
+                DemoHelper.Pause();
+                //driver.FindElement(By.XPath("/html/body/app-root/div/section/app-quote/div[1]/div/div/button")).Click();
+                //Click "Email Quote"
+                DemoHelper.Pause(7000);
+            }
+        }
+    }
+}
